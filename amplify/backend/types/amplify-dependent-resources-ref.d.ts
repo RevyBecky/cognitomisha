f74c1019a81275fd73c5da14c449ec238961a827
MishaKlishin <Misha.Klyshin@cyfroweszkoly.pl>
2024-05-13 17:55:40 +0200

    loginmish

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "ccognmisha15e4c02a": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
